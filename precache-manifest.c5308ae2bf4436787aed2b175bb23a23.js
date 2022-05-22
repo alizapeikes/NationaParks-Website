24ae483ca4295040934e81ab2085f1badb543a8f
alizapeikes <79273849+alizapeikes@users.noreply.github.com>
2022-05-21 23:40:04 -0400

    Updates

---
+++ precache-manifest.c5308ae2bf4436787aed2b175bb23a23.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1ec8abe1fae1aefb149",
+    "url": "/NationaParks-Website/static/css/main.4380a8a6.chunk.css"
+  },
+  {
+    "revision": "c1ec8abe1fae1aefb149",
+    "url": "/NationaParks-Website/static/js/main.c1ec8abe.chunk.js"
+  },
+  {
+    "revision": "0991bc6c139517306fd2",
+    "url": "/NationaParks-Website/static/js/1.0991bc6c.chunk.js"
+  },
+  {
+    "revision": "9fa991be8a5f14de8aec",
+    "url": "/NationaParks-Website/static/js/2.9fa991be.chunk.js"
+  },
+  {
+    "revision": "25e92df98e41736ca972",
+    "url": "/NationaParks-Website/static/js/runtime~main.25e92df9.js"
+  },
+  {
+    "revision": "69e5d800ed9a0619c964c9bf986c6e69",
+    "url": "/NationaParks-Website/static/media/parksvideoReal.69e5d800.mp4"
+  },
+  {
+    "revision": "0f27f8ed0dd58c0dd5516bb1c359521c",
+    "url": "/NationaParks-Website/static/media/weather.0f27f8ed.png"
+  },
+  {
+    "revision": "33467ecb6e6d0f2ee1d4b925ed032cc7",
+    "url": "/NationaParks-Website/static/media/NationalParkPic.33467ecb.png"
+  },
+  {
+    "revision": "9b3907100a8adbd0fc015d98e15c9c42",
+    "url": "/NationaParks-Website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/NationaParks-Website/precache-manifest.9d4d194d8132743e78e993e75a30f015.js"
+  "/NationaParks-Website/precache-manifest.c5308ae2bf4436787aed2b175bb23a23.js"
 );
 
 workbox.clientsClaim();
